1715bc05c0e39c9e46381a5d062ae0b9a1c9520b
piedracita <arnaldpedros@gmail.com>
2025-12-27 23:27:04 +0100

    start of the website

---
+++ 404.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <!-- COMMON-HEAD -->
+        <?php include "common-php/head.html"; ?>
+        <!-- /COMMON-HEAD -->
+
+        <title>404 Page Not Found | Elegant Spanish Guitarist for Destination Weddings in Barcelona, Costa Brava & Sitges</title>
+
+        <meta name="description" content="404 Page Not Found of the Professional Spanish guitarist for destination weddings in Barcelona, Costa Brava, and Sitges. Elegant live music for your ceremony and cocktail hour. Book your authentic Mediterranean soundtrack.">
+        <meta name="subject" content="Spanish Guitarist for Weddings and Events in Barcelona, Costa Brava and Sitges">
+        <meta name="author" content="Spanish Guitar - Benja">
+        
+    </head>
+
+    <body>
+        <!-- COMMON-HEADER/NAVIGATION -->
+        <?php include "common-php/header-navigation/en.html"; ?>
+        <!-- /COMMON-HEADER/NAVIGATION -->
+
+        <!-- Hero -->
+        <section id="slider" class="hero p-0 odd featured">
+            <div class="swiper-container no-slider animation slider-h-50 slider-h-auto">
+                <div class="swiper-wrapper">
+
+                    <!-- Item 1 -->
+                    <div class="swiper-slide slide-center">
+
+                        <!-- Media -->
+                        <img src="assets/images/bg-wide.jpg" alt="Full Image" class="full-image" data-mask="80">
+
+                        <div class="slide-content row text-center">
+                            <div class="col-12 mx-auto inner">
+
+                                <!-- Content -->
+                                <nav data-aos="zoom-out-up" data-aos-delay="800" aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Nothing Found</li>
+                                    </ol>
+                                </nav>
+                                <h1 class="mb-0 title effect-static-text">404 - Nothing Found</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <!-- COMMON-CONTACT -->
+        <?php include "common-php/contact/en.html"; ?>
+        <!-- /COMMON-CONTACT -->
+
+        <!-- COMMON-FOOTER -->
+        <?php include "common-php/footer/en.html"; ?>
+        <!-- /COMMON-FOOTER -->
+
+        <!-- Modal [responsive menu] -->
+        <div id="menu" class="p-0 modal fade" role="dialog" aria-labelledby="menu" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-slideout" role="document">
+                <div class="modal-content full">
+                    <div class="modal-header" data-dismiss="modal">
+                        <i class="icon-close fas fa-arrow-right"></i>
+                    </div>
+                    <div class="menu modal-body">
+                        <div class="row w-100">
+                            <div class="items p-0 col-12 text-center">
+                                <!-- Append [navbar] -->
+                            </div>
+                            <div class="contacts p-0 col-12 text-center">
+                                <!-- Append [navbar] -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- COMMON-BODY-SCRIPTS -->
+        <?php include "common-php/body-scripts.html"; ?>
+        <!-- /COMMON-BODY-SCRIPTS -->
+
+    </body>
+</html>
